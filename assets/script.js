3a3ff4d034c31361d21ff3d9b044f64ef30d61d0
TyGosley <tygosley@gmail.com>
2023-01-07 19:06:32 -0800

    added media query and some variables js

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// Define variables
+
+let welcomeInstructions = document.querySelector("#instructions");
+let startBtn = document.querySelector("#start-button");
+let welcomePage = document.querySelector("welcome-page");
+
+let questionsPage = document.querySelector("#questions-page");
+let askQuestion = document.querySelector("#ask-question");
+
+let selectButtons = document.querySelectorAll(".selection-button");
+let answerBtn1 = document.querySelectorAll("#answer-btn1");
+let answerBtn2 = document.querySelectorAll("#answer-btn2");
+let answerBtn3 = document.querySelectorAll("#answer-btn3");
+let answerBtn4 = document.querySelectorAll("#answer-btn4");
+
+let resultLine = document.querySelector("result-line");
+let scores = document.querySelector("submit-page");
+let finalScore = document.querySelector("final-score");
+let userInitials = document.querySelector("initials");
