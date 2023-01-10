a3246b5a3486ccd3f1cd0ebdbac276db23ff0a48
TyGosley <tygosley@gmail.com>
2023-01-09 17:23:01 -0800

    added event listenters: Output problem in js, line 128, needs addressing

--- assets/script.js
+++ assets/script.js
@@ -26,6 +26,11 @@ let finish = document.querySelector("#finish");
 
 let backButton = document.querySelector("#back-button");
 let clearButton = document.querySelector("#clear-button");
+let timeLeft =document.getElementById("timer");
+let seconds =75;
+let questionIndex = 0;
+let totalScore = 0;
+let questionCount =1;
 
 // Create & define questions here
 // Placeholder questions
@@ -72,11 +77,6 @@ let questionArray = [
     },
 ];
 
-let timeLeft =document.getElementById("timer");
-let seconds =75;
-let questionNumber = 0;
-let totalScore = 0;
-let questionCount =1;
 
 // Write functions here
 // When I click the start button, THEN a time starts.  Use setInterval here
@@ -88,27 +88,27 @@ function countDown() {
 
         if (secondsLeft <= 0) {
             clearInterval(timerInterval);
-            timeLeft.textContent = "Time\'s up!";
-            finish.textContent = "Time\'s up!";
+            timeLeft.textContent = "Time is up!";
+            finish.textContent = "Time is up!";
             gameOver();
-        } else (questionCount >=questionArray.length + 1) { 
+        } else (questionCount >=questionArray.length + 1) 
             clearInterval(timerInterval);
             gameOver();
-        }
+        })
         
-    }, 1000);
-}
+    } 1000;
+
 
 // to start quiz click start button
 function startQuiz() {
     welcomePage.style.display = "none";
     questionsPage.style.display = "block";
-    questionNumber = 0;
+    questionIndex = 0;
     countDown();
-    showQuestion(questionNumber);
+    showQuestion(questionIndex);
 }
-
-function showQuestion (n) {
+// Is this labeled right?
+function showQuestion(n) {
     askQuestion.textContent = questionArray[n].question;
     answerButton1.textContent = questionArray.choices[0];
     answerButton2.textContent = questionArray.choices[1];
@@ -124,17 +124,17 @@ function checkAnswer(event) {
         resultLine.style.display = "none";
     }, 1000);
 }
-
-if (questionArray[questionNumber].answer === event.target.value) {
-    result-resultLine.te = "Correct!!!";
-    totalScore = totalScore + 1;
+// WHY IS THIS NOT WORKING?
+if (questionArray[questionIndex].answer == event.target.value) {
+    resultLine.textContent = "Correct!!!";
+    totalScore = totalScore + 10;
 }else {
     secondsLeft = secondsLeft -10;
     resultLine.textContent = "Incorrect!!!" 
 } 
 
-if (questionNumber < questionArray.length -1) {
-    showQuestion(questionNumber +1);
+if (questionIndex < questionArray.length -1) {
+    showQuestion(questionIndex +1);
 } else {
     gameOver();
 }
@@ -165,10 +165,10 @@ function renderScore () {
     userScore.innerHTML = "";
     userScore.style.display ="block";
     let highScores = sort();   
-    // Slice the high score array to only show the top five high scores. 
-    var topThree = highScores.slice(0,3);
-    for (var i = 0; i < topThree.length; i++) {
-        var item = topThree[i];
+    // Slice the high score array to only show the top 3 scores 
+    let topThree = highScores.slice(0,3);
+    for (let i = 0; i < topThree.length; i++) {
+        let item = topThree[i];
     // Show the score list on score board
     var li = document.createElement("li");
     li.textContent = item.user + " - " + item.score;
@@ -176,3 +176,79 @@ function renderScore () {
     userScore.appendChild(li);
     }
 };
+
+function sortScore () {
+    let unsortedList = getScore();
+    if (getScore === null) {
+        return;
+    } else {
+        unsortedList.sort(function(a,b) {
+            return b.score - a.score;
+
+        })
+        return unsortedList;
+    }
+};
+
+// push new user score/initials to local storage
+function addItem(n) {
+    let addedList = getScore();
+    addedList.push(n);
+    localStorage.setItem("ScoreList", JSON.stringify(addedList));
+};
+
+function saveScore() {
+    let scoreItem = {
+        user: userInitials.value,
+        scores: totalScore
+    }
+    addItem(scoreItem);
+    renderScore();
+}
+
+/* Event listeners here*/
+// startbuttonn to start the quiz
+startButton.addEventListener("click", startQuiz);
+
+//click any choices button, go to the next question
+selectButtons.forEach(function(click) {
+
+    click.addEventListener("click", checkAnswer);
+});
+
+//save information and go to next page
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    scores.style.display = "none";
+    welcomePage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionsPage.style.display ="none";
+    saveScore();
+});
+
+// check highscore ranking list
+finalScore.addEventListener("click", function(event) {
+    event.preventDefault();
+    scores.style.display = "none";
+    welcomPage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionsPage.style.display ="none";
+    renderScore();
+});
+
+//go back to main page
+backButton.addEventListener("click",function(event){
+        event.preventDefault();
+        scores.style.display = "none";
+        welcomePage.style.display = "block";
+        highScorePage.style.display = "none";
+        questionsPage.style.display ="none";
+        location.reload();
+});
+
+//clear local storage and clear page shows
+clearButton.addEventListener("click",function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    renderScore();
+});
